699e743a8eb7f05a69e642a0e6aa37849191375a
Prashant Ranjan <prashant.ranjan2@cybernetyx.com>
2016-09-15 17:45:02 +0530

    cleanup code for android

--- source/java/MyCustomAppActivity.java
+++ source/java/MyCustomAppActivity.java
@@ -12,28 +12,33 @@ public class MyCustomAppActivity extends QtActivity {
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        Log.i("Activity", "Starting service!");
+        Log.i("Activity", "onCreate Activity!");
         Intent serviceIntent = new Intent(this, org.qtproject.example.MyCustomAppService.class);
         startService(serviceIntent);
     }
     @Override
     protected void onResume() {
         super.onResume();
+        Log.i("Activity", "onResume Activity!");
     }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        Log.i("Activity", "onSaveInstanceState Activity!");
     }
     @Override
     public void onPause() {
         super.onPause();
+        Log.i("Activity", "onPause Activity!");
     }
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.i("Activity", "onDestroy Activity!");
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        Log.i("Activity", "onActivityResult Activity!");
     }
 }
--- source/java/MyCustomAppService.java
+++ source/java/MyCustomAppService.java
@@ -13,14 +13,14 @@ public class MyCustomAppService extends QtService {
    @Override
    public void onCreate() {
       super.onCreate();
-      Log.i("Service", "Service created!");
+      Log.i("Service", "Service onCreate!");
    }
 
    /** The service is starting, due to a call to startService() */
    @Override
    public int onStartCommand(Intent intent, int flags, int startId) {
       int ret = super.onStartCommand(intent, flags, startId);
-      Log.i("Service", "Service created!");
+      Log.i("Service", "Service onStartCommand!");
       return ret;
       //return mStartMode;
    }
@@ -28,25 +28,31 @@ public class MyCustomAppService extends QtService {
    /** A client is binding to the service with bindService() */
    @Override
    public IBinder onBind(Intent intent) {
-      return super.onBind(intent);
+      IBinder ret = super.onBind(intent);
+      Log.i("Service", "Service onStartCommand!");
+      return ret;
       //return mBinder;
    }
 
    /** Called when all clients have unbound with unbindService() */
    @Override
    public boolean onUnbind(Intent intent) {
-      return super.onUnbind(intent);
+      boolean ret = super.onUnbind(intent);
+      Log.i("Service", "Service onUnbind!");
+      return ret;
    }
 
    /** Called when a client is binding to the service with bindService()*/
    @Override
    public void onRebind(Intent intent) {
          super.onRebind(intent);
+         Log.i("Service", "Service onRebind!");
    }
 
    /** Called when The service is no longer used and is being destroyed */
    @Override
    public void onDestroy() {
       super.onDestroy();
+      Log.i("Service", "Service onDestroy!");
    }
 }
